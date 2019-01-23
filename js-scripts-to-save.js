a44aad5b9d6c37daa203266f454630e7a6a448fe
kenwrites <kenwrites@mykolab.com>
2019-01-23 12:00:57 -0500

    Adding dynamic variable creation in .js

--- js-scripts-to-save.js
+++ js-scripts-to-save.js
@@ -1,3 +1,9 @@
 // Random number generator - example generates number from 1 to 6
 
 var randomNumber = Math.floor(Math.random() * 6) + 1;
+
+// Create variables with `eval'.  Could be combined with loop to create 
+// variables iteratively.  
+
+var str = "var variable1;"
+eval(str);
