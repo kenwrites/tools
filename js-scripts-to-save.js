7d378fbe181887e64e1288b75269bb2a397a5bc5
kenwrites <kenwrites@mykolab.com>
2019-01-23 19:56:07 -0500

    Add random number generator as function

--- js-scripts-to-save.js
+++ js-scripts-to-save.js
@@ -2,6 +2,15 @@
 
 var randomNumber = Math.floor(Math.random() * 6) + 1;
 
+// Random number generator as function.  Example generates number
+// from 1 to upper.  
+
+function get_random_number(upper) {
+    var random_number = Math.floor(Math.random() * upper) + 1;
+    return random_number;
+}
+
+
 // Create variables with `eval'.  Could be combined with loop to create 
 // variables iteratively.  
 
